ff9281aaca2eb2cedacdf1bf85ce05a4ebc473f1
dang022 <80513676+dang022@users.noreply.github.com>
2024-04-26 17:38:18 +0700

    Update hero banner

--- src/component/frontPage/HeroBanner.js
+++ src/component/frontPage/HeroBanner.js
@@ -120,7 +120,7 @@ function HeroBanner() {
                         <p className="text-white animated slideInLeft mb-4 pb-2">{word3}</p>
                         <NavLink reloadDocument to="/BookingSite" className="btn btn-primary py-sm-3 px-sm-5 animated slideInLeft">Book A Table</NavLink>
                     </div>
-                    <div className="col-lg-6 text-center text-lg-end overflow-hidden lenderX sonFollowRuleX2">
+                    <div className="col-lg-6 text-center text-lg-end lenderX sonFollowRuleX2">
                         <img className="img-fluid" width="70%" height="70%" src={styleA} alt="" />
                     </div>
                 </div>
